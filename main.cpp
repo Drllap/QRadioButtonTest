6a11b534f6adc3b372a777761b1f0f97426eca6c
Pall Palsson <palli@kvikna.com>
2021-02-11 11:16:14 +0000

    Print out the style factory keys to console

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <QApplication>
+#include <QStyleFactory>
+#include <QDebug>
 
 #include "MainWindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    qDebug() << QStyleFactory::keys().join(" ");
     MainWindow mw;
     mw.show();
     return a.exec();
